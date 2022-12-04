1b1f8e659aba5249e3fa213ed30e62f25f711ba7
汪志鹏 <13476859295@163.com>
2022-12-04 23:59:18 +0800

    add login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -30,35 +30,39 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Form, Button, FloatingLabel } from 'react-bootstrap';
 import shareVideo from './share.mp4';
+import './Login.css'
 // import "bootstrap/dist/css/bootstrap.min.css";
 
 function Login() {
   return (
-    <div className="flex justify-start items-center flex-col h-screen">
-      <div className=" relative w-full h-full">
-        <video
-          src={shareVideo}
-          type="video/mp4"
-          loop
-          controls={false}
-          muted
-          autoPlay
-          className="w-full h-full object-cover"
-        />
+    <div className="login-wrap">
+      <div className="login-html">
+        <input id="tab-1" type="radio" name="tab" className="sign-in" checked /><label htmlFor="tab-1" className="tab">Sign In</label>
+        <input id="tab-2" type="radio" name="tab" className="sign-up" /><label htmlFor="tab-2" className="tab">Sign Up</label>
+        <div className="login-form">
+          <div className="sign-in-htm">
+            <div className="group">
+              <label htmlFor="user" className="label">Username</label>
+              <input id="user" type="text" className="input" />
+            </div>
+            <div className="group">
+              <label htmlFor="pass" className="label">Password</label>
+              <input id="pass" type="password" className="input" />
+            </div>
+            <div className="group">
+              <input id="check" type="checkbox" className="check" checked />
+              <label htmlFor="check"><span className="icon" />Keep me signed in </label>
+            </div>
+            <div className="group">
+              <input type="submit" className="button" value="Sign In" />
+            </div>
+            <div className="hr" />
+            <div className="foot-Ink">
+              <a href="#forgot">Forgot Password?</a>
+            </div>
+          </div>
+        </div>
       </div>
-      <Form>
-        <Form.Label>Passenger Count</Form.Label>
-        <Form.Select
-          aria-label="Passenger Count"
-          className="mb-2 form-select-lg"
-          id="passengerCount"
-        >
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
-        </Form.Select>
-      </Form>
     </div>
   );
 }
