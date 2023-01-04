bda40f4ce071449298665e0b759a69f2d44d9b77
汪志鹏 <13476859295@163.com>
2023-01-04 21:33:24 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 //const backendHost = 'https://4939-2401-7400-4002-73dc-6490-dd7c-f837-ab42.ap.ngrok.io '
-const backendHost = 'http://localhost:3001'
+const backendHost = 'https://4939-2401-7400-4002-73dc-6490-dd7c-f837-ab42.ap.ngrok.io'
 export default backendHost
